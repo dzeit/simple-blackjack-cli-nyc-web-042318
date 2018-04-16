1e7bbb83652a5b9c2772f8203bfcea6bc3d9196f
David Zeit <dzeit216@gmail.com>
2018-04-16 22:56:13 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,7 +55,9 @@ end
 
 def runner
   welcome
-  initial_round
-  hit? + display_card_total <= 21
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+  end
   end_game
 end
